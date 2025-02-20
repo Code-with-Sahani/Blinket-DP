e1caa9040f72c5a6f53c58624d1d98c709b8e2aa
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-02-21 01:12:38 +0530

    Add files via upload

---
+++ src/Components/DP_Not_Answer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const DP_Not_Answer = () => {
+  return (
+    <div className='m-2 p-4 text-sm bg-gray-800 text-white rounded-md md:p-6 lg:p-8'>
+    <h2 className="text-center text-2xl font-semibold text-emerald-400 mb-1"> DP Not Answering </h2>
+
+    <div className='space-y-4'>
+ 
+        <div className="p-3 bg-gray-700 rounded-md shadow">
+            <ul className="list-disc pl-5">
+
+                <li className='p-2'>Dp Not Responding -- Call Twice -- Not responsive </li>
+                <li className='p-2'>Dp Not Responding  -- previous Agent Call Twice -- hence Call Once -- Not Answering</li>
+
+            </ul>
+        </div>
+ 
+
+    </div>
+
+</div>
+  )
+}
+
+export default DP_Not_Answer
