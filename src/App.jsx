0a8cb7655334324ac4f702bbe0e2d74745847ff6
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-02-21 01:00:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import CDO from './Components/CDO'
 
 const App = () => {
   return (
-    <div className='h-full w-full bg-gray-900 text-white'>
+    <div className='h-full w-full bg-gray-900 p-8 text-white'>
       <header className=' p-2 text-pink-500 text-center text-2xl font-semibold' > Rider Support Comment Sheet - Certified by "Chahat Tharwani"</header>
       {/* <AddressIssue/> */}
       <CNR/>
