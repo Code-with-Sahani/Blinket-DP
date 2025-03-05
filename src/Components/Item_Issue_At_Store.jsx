d507711a8dbdbadaae8abad1ac49b8fee5194d1e
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-03-05 11:39:21 +0530

    Add files via upload

---
+++ src/Components/Item_Issue_At_Store.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Item_Issue_At_Store = () => {
+  return (
+    <div className="m-2 p-4 text-sm bg-gray-800 text-white rounded-md md:p-6 lg:p-8">
+            <h2 className='text-center text-2xl font-semibold text-emerald-400 mb-1'>ITem issue is in Processing....</h2>
+            
+            <div className="space-y-4">
+                <div className="p-3 bg-gray-700 rounded-md shadow">
+                    <ul className="list-disc pl-5">
+ 
+
+                    </ul>
+                </div>
+
+
+
+            </div>
+
+
+        </div>
+  )
+}
+
+export default Item_Issue_At_Store
