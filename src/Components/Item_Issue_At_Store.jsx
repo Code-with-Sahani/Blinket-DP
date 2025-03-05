4d08f63da2be44ae0b82ee3fbcdbe9a8c0789591
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-03-05 11:40:48 +0530

    Update Item_Issue_At_Store.jsx

--- src/Components/Item_Issue_At_Store.jsx
+++ src/Components/Item_Issue_At_Store.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Item_Issue_At_Store = () => {
   return (
     <div className="m-2 p-4 text-sm bg-gray-800 text-white rounded-md md:p-6 lg:p-8">
-            <h2 className='text-center text-2xl font-semibold text-emerald-400 mb-1'>ITem issue is in Processing....</h2>
+            <h2 className='text-center text-2xl font-semibold text-emerald-400 mb-1'>Item issue is in Processing....</h2>
             
             <div className="space-y-4">
                 <div className="p-3 bg-gray-700 rounded-md shadow">
