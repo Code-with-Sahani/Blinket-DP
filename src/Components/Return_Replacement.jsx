38ff6ca34e3a7b8fd4da0f72d8fa2089f428b439
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-03-05 11:33:26 +0530

    Add files via upload

---
+++ src/Components/Return_Replacement.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Return_Replacement = () => {
+    return (
+        <div className="m-2 p-4 text-sm bg-gray-800 text-white rounded-md md:p-6 lg:p-8">
+            <h2 className='text-center text-2xl font-semibold text-emerald-400 mb-1'>Return / Replacement</h2>
+
+            <div className="space-y-4">
+                <div className="p-3 bg-gray-700 rounded-md shadow">
+                    <ul className="list-disc pl-5">
+
+                        <li className='p-2'>Pickup order-CNR -- called cx -- <strong className='text-red-400 font-semibold'>Cx responsive</strong> -- share same to DP & ensure pickup </li>
+
+                        <li className="p-2">Pickup order-CNR -- called cx -- <strong className='text-red-400 font-semibold'>Cx not responsive</strong> -- order cancel -- same share to DP</li>
+                       
+                        <li className="p-2">Pickup order-CDO -- called cx -- <strong className='text-red-400 font-semibold'>Cx responsive</strong> -- cx do not want to return -- order cancel -- share same to DP</li>
+
+                        <li className="p-2">Pickup order -- DP denied due to bike issue -- called cx -- <strong className='text-red-400 font-semibold'>Cx responsive</strong> -- same share to Cx -- order cancel -- same share to DP</li>
+
+                    </ul>
+                </div>
+
+
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Return_Replacement
