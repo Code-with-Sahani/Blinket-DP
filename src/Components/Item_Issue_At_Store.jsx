fc2d3cf5ac4b0daa7b55b9a48174c2077367e7f3
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-03-06 11:33:57 +0530

    Update Item_Issue_At_Store.jsx

--- src/Components/Item_Issue_At_Store.jsx
+++ src/Components/Item_Issue_At_Store.jsx
@@ -3,22 +3,26 @@ import React from 'react'
 const Item_Issue_At_Store = () => {
   return (
     <div className="m-2 p-4 text-sm bg-gray-800 text-white rounded-md md:p-6 lg:p-8">
-            <h2 className='text-center text-2xl font-semibold text-emerald-400 mb-1'>Item issue is in Processing....</h2>
-            
-            <div className="space-y-4">
-                <div className="p-3 bg-gray-700 rounded-md shadow">
-                    <ul className="list-disc pl-5">
- 
+      <h2 className='text-center text-2xl font-semibold text-emerald-400 mb-1'>Item issue at store</h2>
 
-                    </ul>
-                </div>
+      <div className="space-y-4">
+        <div className="p-3 bg-gray-700 rounded-md shadow">
+          <ul className="list-disc pl-5">
 
-
-
-            </div>
+            <li className='p-2'>Item issue at store -- full item out of stock -- <strong className='text-yellow-300 font-semibold'>billed</strong> -- ask to connect SM</li>
+            <li className='p-2'>Item issue at store -- full item out of stock -- <strong className='text-yellow-300 font-semibold'>not billed</strong> -- ask rider to ask picker to mark unavailable that item</li>
+            <li className='p-2'>Item issue at store -- Partial item out of stock -- ask rider to take the partial item -- go for the deloivery if there is any issue then reconnect us again</li>
 
 
+          </ul>
         </div>
+
+
+
+      </div>
+
+
+    </div>
   )
 }
 
