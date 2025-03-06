320738d77fb4c0727384a279610ab0df57d65aa2
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-03-06 11:21:35 +0530

    Update AddressIssue.jsx

--- src/Components/AddressIssue.jsx
+++ src/Components/AddressIssue.jsx
@@ -11,9 +11,9 @@ const AddressIssue = () => {
                     <ul className="list-disc pl-5">
                         <li className="p-2">Out of polygon -- <strong className="text-yellow-400 font-semibold">Alternate Store</strong> -- Ask DP to deliver order & denied cancellation</li>
                         <li className="p-2">Out of polygon -- <strong className="text-yellow-400 font-semibold">Serviceable</strong> -- Ask DP to deliver order & denied cancellation</li>
-                        <li className="p-2">Out of polygon -- <strong className="text-yellow-400 font-semibold">Serviceable</strong> -- 2.2 Km -- pitch extra payout -- Ask DP to deliver order & denied cancellation</li>
-                        <li className="p-2">Out of polygon -- <strong className="text-yellow-400 font-semibold">Not Serviceable</strong> -- 2.9 Km -- pitch extra payout -- DP agree -- Ask DP to deliver order & denied cancellation </li>
-                        <li className="p-2">Out of polygon -- <strong className="text-yellow-400 font-semibold">Not Serviceable</strong> -- 2.9 Km -- pitch extra payout -- DP Disagree -- Call Cx for cancellation & refund details -- Order cancel by me -- Same inform to the DP</li>
+                        <li className="p-2">Out of polygon -- <strong className="text-yellow-400 font-semibold">Serviceable</strong> -- 2.2 km from pin location -- pitch extra payout -- Ask DP to deliver order & denied cancellation</li>
+                        <li className="p-2">Out of polygon -- <strong className="text-yellow-400 font-semibold">Not Serviceable</strong> -- 2.9 km from pin location -- pitch extra payout -- DP agree -- Ask DP to deliver order & denied cancellation </li>
+                        <li className="p-2">Out of polygon -- <strong className="text-yellow-400 font-semibold">Not Serviceable</strong> -- 2.9 km from pin location -- pitch extra payout -- DP Disagree -- Call Cx for cancellation & refund details -- Order cancel by me -- Same inform to the DP</li>
                     </ul>
                 </div>
 
@@ -23,16 +23,16 @@ const AddressIssue = () => {
                     <h3 className="text-lg font-semibold text-emerald-400 mb-3">Address Issue-Pin location / address mismatch</h3>
                     <ul className="list-disc pl-5">
                         <li className="p-2">
-                            Address Issue-Pin location/ address mismatch -- <strong className="text-yellow-400 font-semibold">Serviceable</strong> -- 1.7 km  -- pitch extra payout -- ask DP to delivered on address
+                            Address Issue-Pin location/ address mismatch -- <strong className="text-yellow-400 font-semibold">Serviceable</strong> -- 1.7 km from pin location  -- pitch extra payout -- ask DP to delivered on address
                         </li>
 
-                        <li className='p-2'>Address Issue-Pin location/ address mismatch -- <strong className="text-yellow-400 font-semibold">Not Serviceable</strong> -- 350 m -- pitch extra payout -- DP agree -- ask DP to deliver order & denied cancellation
+                        <li className='p-2'>Address Issue-Pin location/ address mismatch -- <strong className="text-yellow-400 font-semibold">Not Serviceable</strong> -- 350 m from pin location -- pitch extra payout -- DP agree -- ask DP to deliver order & denied cancellation
                         </li>
 
-                        <li className='p-2'>Pin location/ address mismatch/ Location issue -- <strong className="text-yellow-400 font-semibold">Not Serviceable</strong> -- 350 m -- pitch extra payout -- DP agree -- called cx -- <strong className="text-red-400 font-semibold">cx responsive</strong> -- requested cx to called DP for the better location guidance -- ask DP to deliver order
+                        <li className='p-2'>Pin location/ address mismatch/ Location issue -- <strong className="text-yellow-400 font-semibold">Not Serviceable</strong> -- 350 m from pin location -- pitch extra payout -- DP agree -- called cx -- <strong className="text-red-400 font-semibold">cx responsive</strong> -- requested cx to called DP for the better location guidance -- ask DP to deliver order
                         </li>
 
-                        <li className='p-2'>Address Issue-Pin location/ address mismatch -- <strong className="text-yellow-400 font-semibold">Not Serviceable</strong> -- 5.1 Km -- pitch extra payout -- DP disagree -- call Cx for cancellation & refund details -- Order cancel by me -- Same inform to the DP</li>
+                        <li className='p-2'>Address Issue-Pin location/ address mismatch -- <strong className="text-yellow-400 font-semibold">Not Serviceable</strong> -- 5.1 km from pin location -- pitch extra payout -- DP disagree -- call Cx for cancellation & refund details -- Order cancel by me -- Same inform to the DP</li>
                        
                     </ul>
 
@@ -40,7 +40,7 @@ const AddressIssue = () => {
 
                 <div className="p-3 bg-gray-700 rounded-md shadow">
                     <h3 className="text-lg font-semibold text-emerald-400 mb-3">Address Issue-Pin location is Far/ Wrong Address </h3>
-                    <ul className="list-disc pl-5">
+                    <ul className="list-disc pl-5 text-amber-200" >
                         <li className="p-2">DP Said wrong address -- <strong className="text-yellow-400 font-semibold">Serviceable</strong> -- called cx -- <strong className="text-red-400 font-semibold">cx responsive</strong> -- push back given -- but cx ready to accept -- same share to DP</li>
 
                         <li className="p-2">DP Said wrong address -- <strong className="text-yellow-400 font-semibold">Serviceable</strong> -- called cx -- <strong className="text-red-400 font-semibold">cx responsive</strong> -- push back given -- but cx not ready -- shared cancellation charges -- order cancel -- share same to DP</li>
@@ -73,30 +73,19 @@ const AddressIssue = () => {
                         </li>
                     </ul>
                 </div>
-
-
                 <div className="p-3 bg-gray-700 rounded-md shadow">
                     <h3 className="text-lg font-semibold text-emerald-400 mb-3">Incomplete Address</h3>
                     <ul className="list-disc pl-5">
                         <li className="p-2">Incomplete address -- mentioned address minor details missing -- called cx -- <strong className="text-red-400 font-semibold">cx responsive </strong> -- requested cx to call the rider for guidance -- same share to rider and asked to ensure delivery
                         </li>
-
                         <li className='p-2'>Incomplete address -- mentioned address minor details missing  -- call cx -- <strong className="text-red-400 font-semibold">cx not responsive</strong> -- requested the rider for few more attempt and asked to wait -- asked to follow the automation process after ETA+10mins-- DP agreed
                         </li>
-
                         <li className='p-2'>Incomplete address -- mentioned address minor details missing  -- call cx -- <strong className="text-red-400 font-semibold">cx not responsive</strong> -- CNR attempt available -- order canceled -- shared the same with DP
                         </li>
-
                     </ul>
-                </div>
-
-               
+                </div>  
             </div>
-
-
         </div>
-
-
     );
 };
 
