18f9c728a4a75500ace0259e9629de86fc39565b
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-03-09 11:39:56 +0530

    Update Return_Replacement.jsx

--- src/Components/Return_Replacement.jsx
+++ src/Components/Return_Replacement.jsx
@@ -9,11 +9,13 @@ const Return_Replacement = () => {
                 <div className="p-3 bg-gray-700 rounded-md shadow">
                     <ul className="list-disc pl-5">
 
-                        <li className='p-2'>Pickup order/ Redelivery-CNR -- called cx -- <strong className='text-red-400 font-semibold'>Cx responsive</strong> -- share same to DP & ensure pickup </li>
+                        <li className='p-2'>Pickup order/ Redelivery-CNR -- called cx -- <strong className='text-red-400 font-semibold'>Cx responsive</strong> -- share same to DP & ensure pickup/Redelivery </li>
 
                         <li className="p-2">Pickup order/ Redelivery-CNR -- called cx -- <strong className='text-red-400 font-semibold'>Cx not responsive</strong> -- order cancel -- same share to DP</li>
                        
                         <li className="p-2">Pickup order/ Redelivery-CDO -- called cx -- <strong className='text-red-400 font-semibold'>Cx responsive</strong> -- cx do not want to return -- order cancel -- share same to DP</li>
+
+                        <li className="p-2">Pickup order/ Redelivery-CDO -- called cx -- <strong className='text-red-400 font-semibold'>Cx responsive</strong> -- cx created wrong pickup/Redelivery return -- order cancel -- ask cx to create the return by selecting correct reason -- share same to DP</li>
 
                         <li className="p-2">Pickup order/ Redelivery -- DP denied due to bike issue -- called cx -- <strong className='text-red-400 font-semibold'>Cx responsive</strong> -- same share to Cx -- order cancel -- same share to DP</li>
 
