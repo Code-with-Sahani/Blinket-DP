f9fb0bf4696db96ad75f26509e5bdeb9244847a9
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-03-15 10:58:27 +0530

    Update UMD.jsx

--- src/Components/UMD.jsx
+++ src/Components/UMD.jsx
@@ -44,25 +44,30 @@ const UMD = () => {
             <div className="space-y-4">
                 <div className="p-3 bg-gray-700 rounded-md shadow">
                     <ul className="list-disc pl-5">
+
+                        <li className="p-2">
+                            UMD -- <strong className="text-yellow-300 font-semibold"> DP not marked Enroute not Geo-fence </strong> -- ask DP to first mark enroute then reach Geo-fence --  then try to mark delivered if any issue then connect us again -- DP agreed
+                        </li>
+
                         <li className="p-2">
                             UMD -- <strong className="text-yellow-300 font-semibold">Geo-fence not reached</strong> -- ask to reached at Geo-fence -- then try to mark delivered if any issue then connect us again -- DP agreed
                         </li>
                         <li className="p-2">
                             UMD -- <strong className="text-yellow-300 font-semibold">Geo-fence not reached</strong> -- DP requested to call cx -- denied to call bcos Geo-fence not reached -- ask to reached -- then try to mark delivered if any issue then connect us again -- DP agreed
                         </li>
+
                     </ul>
                 </div>
 
                 <div className="p-3 bg-gray-700 rounded-md shadow">
                     <ul className="list-disc pl-5">
                         <li className="p-2">
-                            UMD -- <strong className="text-yellow-300 font-semibold">Network/ Forget to Mark/ Device issue</strong> -- Geo-fence reached -- call cx for confirmation -- get confirmed -- mark delivered -- share same to the DP
-                        </li>
-                        
-                          <li className="p-2">
                             UMD -- <strong className="text-yellow-300 font-semibold">Network/ Forget to Mark/ Device issue</strong> -- Geo-fence reached -- call cx for confirmation -- cx not confirmed -- denied to mark -- asked rider to go back to the cx location and try to mark the order as delivered
                         </li>
 
+                        <li className="p-2">
+                            UMD -- <strong className="text-yellow-300 font-semibold">Network/ Forget to Mark/ Device issue</strong> -- Geo-fence reached -- call cx for confirmation -- get confirmed -- mark delivered -- share same to the DP
+                        </li>
                         <li className="p-2">
                             UMD -- <strong className="text-yellow-300 font-semibold">Network/ Forget to Mark/ Device issue</strong> -- Geo-fence reached -- call cx for confirmation -- <strong className="text-red-400 font-semibold">cx not responsive</strong> -- denied to mark -- asked rider to go back to the cx location and try to mark the order as delivered
                         </li>
