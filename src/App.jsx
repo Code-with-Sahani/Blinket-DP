0c12f055a1e195bd0721a3094f3a608e5857356f
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-03-17 23:33:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,17 +15,51 @@ const App = () => {
   return (
     <div className='h-full w-full bg-gray-900 text-white'>
       <header className=' p-4 text-pink-200 text-center text-2xl font-semibold' >Comment Sheet For DP support - Certified by "Sahani 	&#128517;"</header>
-      <DP_Not_Answer/>
-      <CNR/>
-      <CDO/>
-      <Item_Issue_At_Store/>
-      <UMD/>
-      <OTP/>
-      <UD_SOS/>
-      <PaymentIssue/>
-      <BuddyRider/>
-      <Return_Replacement/>
-      <AddressIssue/>
+      <details>
+        <summary>DP is not Responding</summary>
+        <DP_Not_Answer />
+      </details>
+      <details>
+        <summary>CNR</summary>
+        <CNR />
+      </details>
+      <details>
+        <summary>CDO</summary>
+        <CDO />
+      </details>
+      <details>
+        <summary>Item issue at Store</summary>
+        <Item_Issue_At_Store />
+      </details>
+      <details>
+        <summary>UMD</summary>
+        <UMD />
+      </details>
+      <details>
+        <summary>OTP issue</summary>
+        <OTP />
+      </details>
+      <details>
+        <summary>Rider Deny to Deliver</summary>
+        <UD_SOS />
+      </details>
+      <details>
+        <summary>Payment Issue</summary>
+        <PaymentIssue />
+      </details>
+      <details>
+        <summary>Buddy RIder assignment</summary>
+        <BuddyRider />
+      </details>
+      <details>
+        <summary>Pickup/ Redelivery</summary>
+        <Return_Replacement />
+      </details>
+      <details>
+        <summary>Address issue</summary>
+        <AddressIssue />
+      </details>
+
     </div>
   )
 }
