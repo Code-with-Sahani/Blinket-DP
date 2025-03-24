731483a8089cb41d94266c2f1de6d7d623c212bc
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-03-24 15:02:42 +0530

    Update UMD.jsx

--- src/Components/UMD.jsx
+++ src/Components/UMD.jsx
@@ -12,13 +12,13 @@ const UMD = () => {
                 <div className="p-3 bg-gray-700 rounded-md shadow">
                     <ul className="list-disc pl-5">
                         <li className="p-2">
-                            UMD -- <strong className="text-yellow-300 font-semibold">Pin location/address mismatch or location issue</strong> -- call cx for confirmation -- get confirmed -- mark delivered -- share same to the DP
+                            UMD — <strong className="text-yellow-300 font-semibold">Pin location/address mismatch or location issue</strong> — call cx for confirmation — get confirmed — mark delivered — share same to the DP
                         </li>
                         <li className="p-2">
-                            UMD -- <strong className="text-yellow-300 font-semibold">Pin location/address mismatch or location issue</strong> -- call cx for confirmation -- cx not confirmed -- denied to mark -- asked rider to go back to the cx location and try to mark the order as delivered
+                            UMD — <strong className="text-yellow-300 font-semibold">Pin location/address mismatch or location issue</strong> — call cx for confirmation — cx not confirmed — denied to mark — asked rider to go back to the cx location and try to mark the order as delivered
                         </li>
                         <li className="p-2">
-                            UMD -- <strong className="text-yellow-300 font-semibold">Pin location/address mismatch or location issue</strong> -- call cx for confirmation -- <strong className="text-red-400 font-semibold">cx not responsive</strong> -- denied to mark -- asked rider to go back to the cx location and try to mark the order as delivered
+                            UMD — <strong className="text-yellow-300 font-semibold">Pin location/address mismatch or location issue</strong> — call cx for confirmation — <strong className="text-red-400 font-semibold">cx not responsive</strong> — denied to mark — asked rider to go back to the cx location and try to mark the order as delivered
                         </li>
                     </ul>
                 </div>
@@ -26,14 +26,14 @@ const UMD = () => {
                 <div className="p-3 bg-gray-700 rounded-md shadow">
                     <ul className="list-disc pl-5">
                         <li className="p-2">
-                            UMD -- <strong className="text-yellow-300 font-semibold">Network/ Forget to Mark/ Device issue -- low confidence</strong> -- call cx for confirmation -- get confirmed -- mark delivered -- share same to the DP
+                            UMD — <strong className="text-yellow-300 font-semibold">Network/ Forget to Mark/ Device issue — low confidence</strong> — call cx for confirmation — get confirmed — mark delivered — share same to the DP
                         </li>
 
                         <li className="p-2">
-                            UMD -- <strong className="text-yellow-300 font-semibold">Network/ Forget to Mark/ Device issue -- low confidence</strong> -- call cx for confirmation -- cx not confirmed -- denied to mark -- asked rider to go back to the cx location and try to mark the order as delivered
+                            UMD — <strong className="text-yellow-300 font-semibold">Network/ Forget to Mark/ Device issue — low confidence</strong> — call cx for confirmation — cx not confirmed — denied to mark — asked rider to go back to the cx location and try to mark the order as delivered
                         </li>
                         <li className="p-2">
-                            UMD -- <strong className="text-yellow-300 font-semibold">Network/ Forget to Mark/ Device issue -- low confidence</strong> -- call cx for confirmation -- <strong className="text-red-400 font-semibold">cx not responsive</strong> -- denied to mark -- asked rider to go back to the cx location and try to mark the order as delivered
+                            UMD — <strong className="text-yellow-300 font-semibold">Network/ Forget to Mark/ Device issue — low confidence</strong> — call cx for confirmation — <strong className="text-red-400 font-semibold">cx not responsive</strong> — denied to mark — asked rider to go back to the cx location and try to mark the order as delivered
                         </li>
                     </ul>
                 </div>
@@ -46,14 +46,14 @@ const UMD = () => {
                     <ul className="list-disc pl-5">
 
                         <li className="p-2">
-                            UMD -- <strong className="text-yellow-300 font-semibold"> DP not marked Enroute not Geo-fence </strong> -- ask DP to first mark enroute then reach Geo-fence --  then try to mark delivered if any issue then connect us again -- DP agreed
+                            UMD — <strong className="text-yellow-300 font-semibold"> DP not marked Pickup nor Geo-fence </strong> — ask DP to first mark Pickup then reach Geo-fence — then try to mark delivered if any issue then connect us again — DP agreed
                         </li>
 
                         <li className="p-2">
-                            UMD -- <strong className="text-yellow-300 font-semibold">Geo-fence not reached</strong> -- ask to reached at Geo-fence -- then try to mark delivered if any issue then connect us again -- DP agreed
+                            UMD — <strong className="text-yellow-300 font-semibold">Geo-fence not reached</strong> — ask to reached at Geo-fence — then try to mark delivered if any issue then connect us again — DP agreed
                         </li>
                         <li className="p-2">
-                            UMD -- <strong className="text-yellow-300 font-semibold">Geo-fence not reached</strong> -- DP requested to call cx -- denied to call bcos Geo-fence not reached -- ask to reached -- then try to mark delivered if any issue then connect us again -- DP agreed
+                            UMD — <strong className="text-yellow-300 font-semibold">Geo-fence not reached</strong> — DP requested to call cx — denied to call bcos Geo-fence not reached — ask to reached — then try to mark delivered if any issue then connect us again — DP agreed
                         </li>
 
                     </ul>
@@ -61,17 +61,31 @@ const UMD = () => {
 
                 <div className="p-3 bg-gray-700 rounded-md shadow">
                     <ul className="list-disc pl-5">
+                        <li className="p-2">
+                            UMD — <strong className="text-yellow-300 font-semibold">Network/ Forget to Mark/ Device issue</strong> — Geo-fence reached — call cx for confirmation — cx not confirmed — denied to mark — asked rider to go back to the cx location and try to mark the order as delivered
+                        </li>
 
                         <li className="p-2">
-                            UMD -- <strong className="text-yellow-300 font-semibold">Network/ Forget to Mark/ Device issue</strong> -- Geo-fence reached -- call cx for confirmation -- get confirmed -- mark delivered -- share same to the DP
+                            UMD — <strong className="text-yellow-300 font-semibold">Network/ Forget to Mark/ Device issue</strong> — Geo-fence reached — call cx for confirmation — get confirmed — mark delivered — share same to the DP
                         </li>
+                        <li className="p-2">
+                            UMD — <strong className="text-yellow-300 font-semibold">Network/ Forget to Mark/ Device issue</strong> — Geo-fence reached — call cx for confirmation — <strong className="text-red-400 font-semibold">cx not responsive</strong> — denied to mark — asked rider to go back to the cx location and try to mark the order as delivered
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="p-3 bg-gray-700 rounded-md shadow">
+                <h3 className="text-lg font-semibold text-emerald-500 mt-5 mb-3">Other scenrios Or Rare scenrios</h3>
+                    <ul className="list-disc pl-5">
+                        <li className="p-2">Payment issue but payment successful, but UMD — Geo-fence reached — call cx for confirmation — get confirmed — mark delivered — share same to the DP
+                        </li>
+ 
+                        <li className="p-2">Payment issue but payment successful, but UMD — Geo-fence reached — call cx for confirmation — cx not confirmed — denied to mark — asked rider to mark the order as delivered from their end if he delivered
+                        </li>
+
+                        <li className="p-2">UMD — BR — share the status with him, i.e. Delivered already marked by the main rider — ask him to mark delivered itself by reaching location Or Co-odrinate SM for the better help </li>
+
                         
-                        <li className="p-2">
-                            UMD -- <strong className="text-yellow-300 font-semibold">Network/ Forget to Mark/ Device issue</strong> -- Geo-fence reached -- call cx for confirmation -- cx not confirmed -- denied to mark -- asked rider to go back to the cx location and try to mark the order as delivered
-                        </li>
-                        <li className="p-2">
-                            UMD -- <strong className="text-yellow-300 font-semibold">Network/ Forget to Mark/ Device issue</strong> -- Geo-fence reached -- call cx for confirmation -- <strong className="text-red-400 font-semibold">cx not responsive</strong> -- denied to mark -- asked rider to go back to the cx location and try to mark the order as delivered
-                        </li>
                     </ul>
                 </div>
             </div>
