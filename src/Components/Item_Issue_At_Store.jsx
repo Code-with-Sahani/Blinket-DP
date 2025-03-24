5eaaa0f0d65c178218a48497a5496b8484a6686f
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-03-24 13:53:08 +0530

    Update Item_Issue_At_Store.jsx

--- src/Components/Item_Issue_At_Store.jsx
+++ src/Components/Item_Issue_At_Store.jsx
@@ -8,12 +8,12 @@ const Item_Issue_At_Store = () => {
       <div className="space-y-4">
         <div className="p-3 bg-gray-700 rounded-md shadow">
           <ul className="list-disc pl-5">
-
-            <li className='p-2'>Item issue at store -- full item out of stock -- <strong className='text-yellow-300 font-semibold'>billed</strong> -- ask to connect SM</li>
-            <li className='p-2'>Item issue at store -- full item out of stock -- <strong className='text-yellow-300 font-semibold'>not billed</strong> -- ask rider to ask picker to mark unavailable that item</li>
-            <li className='p-2'>Item issue at store -- Partial item out of stock -- ask rider to take the partial item -- go for the deloivery if there is any issue then reconnect us again</li>
-
-
+            <li className='p-2'>Item Issues at Store — not getting order from the store — guided to SM</li>
+            <li className='p-2'>Item issue at store — Partial item damage — ask rider to take the item which he received — go for the delivery if there is any issue, then reconnect us again</li>
+            <hr className='mb-5 mt-5' />
+            <li className='p-2'>Item issue at store — full item out of stock — <strong className='text-yellow-300 font-semibold'>billed</strong> — ask to connect SM</li>
+            <li className='p-2'>Item issue at store — full item out of stock — <strong className='text-yellow-300 font-semibold'>not billed</strong> — ask rider to ask picker to mark unavailable that item</li>
+            <li className='p-2'>Item issue at store — Partial item out of stock — ask rider to take the partial item — go for the deloivery if there is any issue then reconnect us again</li>
           </ul>
         </div>
 
