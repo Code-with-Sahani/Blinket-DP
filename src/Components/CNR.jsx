347fea4c65e305782e86396bcfa182fd90710d0f
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-03-26 10:51:59 +0530

    Update CNR.jsx

--- src/Components/CNR.jsx
+++ src/Components/CNR.jsx
@@ -6,6 +6,18 @@ const CNR = () => {
 
             {/* Main Heading */}
             <h2 className="text-center text-2xl font-semibold text-emerald-400 mb-8">CNR - Customer Not Responsive</h2>
+            {/* Initial Case - Geo-Fence Not Reached */}
+            <div className="p-3 bg-gray-700 rounded-md shadow">
+                <ul className="list-disc pl-5">
+                    <li className="p-2">
+                        CNR — <strong className="text-yellow-300 font-semibold">Geo-Fence not reached</strong> — ask to reach there, then call the customer.
+                    </li>
+
+                    <li className="p-2">
+                        CNR — DP not marked <strong className='text-yellow-300 font-semibold'>pickup nor Geo-fence </strong> — ask DP to first mark pickup then reach Geo-fence — then try to call cx if any issue then connect us again — DP agreed
+                    </li>
+                </ul>
+            </div>
             {/* Geo-Fence Reached Before ETA + 10 min */}
             <h3 className="text-lg font-semibold text-amber-400 mt-5 mb-3">Geo-fence Reached Before (ETA + 10 min)</h3>
             <div className="space-y-4">
