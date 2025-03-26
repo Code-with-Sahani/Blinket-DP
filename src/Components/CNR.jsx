3ab759247dd9711625a243459a4923288d013684
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-03-26 10:50:52 +0530

    Update CNR.jsx

--- src/Components/CNR.jsx
+++ src/Components/CNR.jsx
@@ -6,43 +6,6 @@ const CNR = () => {
 
             {/* Main Heading */}
             <h2 className="text-center text-2xl font-semibold text-emerald-400 mb-8">CNR - Customer Not Responsive</h2>
-
-            {/* Initial Case - Geo-Fence Not Reached */}
-           
-            {/* Geo-Fence Not Marked But DP on Location After ETA + 10 min */}
-            <h3 className="text-lg font-semibold text-amber-400 mt-5 mb-3">Geo-fence Not Marked But DP on Cx's Location After (ETA + 10 min)</h3>
-            <div className="space-y-4">
-                <div className="p-3 bg-gray-700 rounded-md shadow">
-                    <ul className="list-disc pl-5">
-                    <li className="p-2">
-                            DP unable to call cx — <strong className="text-yellow-300 font-semibold">Geo-Fence not reached but near Cx's location</strong> — ETA+10 mins crossed — asked to follow the automation process after ETA+10 mins.
-                        </li>
-                        <li className="p-2">
-                            DP unable to call cx — <strong className="text-yellow-300 font-semibold">Geo-Fence not reached but near Cx's location</strong> — ETA+10 mins crossed — DP requested to call Cx — call Cx — <strong className='text-red-400 font-semibold'>Cx responsive</strong>  — informed cx to collect the order from the rider waiting outside their home — call you if they can't find you — same shared with rider.
-                        </li>
-                        <li className="p-2">
-                            DP unable to call cx — <strong className="text-yellow-300 font-semibold">Geo-Fence not reached but near Cx's location</strong> — ETA+10 mins crossed — DP requested to call Cx — call Cx — <strong className='text-red-400 font-semibold'>Cx not responsive</strong>  — asked to follow the automation process after ETA+10 mins.
-                        </li>
-                        <hr className='mb-5 mt-5' />
-                        <li className="p-2">
-                            CNR — <strong className="text-yellow-300 font-semibold">Geo-Fence not reached but near Cx's location</strong> — ETA+10 mins crossed — CNR attempt available — call Cx twice — <strong className='text-red-400 font-semibold'>Cx responsive</strong> — shared the same with DP.
-                        </li>
-                        <li className="p-2">
-                            CNR — <strong className="text-yellow-300 font-semibold">Geo-Fence not reached but near Cx's location</strong> — ETA+10 mins crossed — CNR attempt available — call Cx twice — <strong className='text-red-400 font-semibold'>Cx not responsive</strong> — order canceled — shared the same with DP.
-                        </li>
-                        <li className="p-2">
-                            CNR — <strong className="text-yellow-300 font-semibold">Geo-Fence not reached but near Cx's location</strong> — ETA+10 mins crossed — CNR attempt not available — asked to follow the automation process.
-                        </li>
-                        <li className="p-2">
-                            CNR — <strong className="text-yellow-300 font-semibold">Geo-Fence not reached but near Cx's location</strong> — ETA+10 mins crossed — CNR attempt not available — DP requested to call Cx — call Cx — <strong className='text-red-400 font-semibold'>Cx responsive</strong> — shared the same with DP.
-                        </li>
-                        <li className="p-2">
-                            CNR — <strong className="text-yellow-300 font-semibold">Geo-Fence not reached but near Cx's location</strong> — ETA+10 mins crossed — CNR attempt not available — DP requested to call Cx — call Cx — <strong className='text-red-400 font-semibold'>Cx not responsive</strong> — asked to follow the automation process.
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
             {/* Geo-Fence Reached Before ETA + 10 min */}
             <h3 className="text-lg font-semibold text-amber-400 mt-5 mb-3">Geo-fence Reached Before (ETA + 10 min)</h3>
             <div className="space-y-4">
