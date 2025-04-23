8748af5199cdef4850fa04f0fa8e0c737d5c4274
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-04-24 00:01:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   return (
     <div className='h-full w-full bg-blue-500 text-white'>
 
-      <header className=' p-4 text-pink-200 text-center text-2xl font-semibold flex gap-100' >Comment Sheet For DP support - Certified by "Sahani 	&#128517;" <h2 className='text-sm mt-3'>Last Updated :23 -04-2025 at 23:30:00</h2></header>
+      <header className=' p-4 text-pink-200 text-center text-2xl font-semibold flex gap-100' >Comment Sheet For DP support - Certified by "Sahani 	&#128517;" <h2 className='text-sm mt-2'>Last Updated :23 -04-2025 at 23:30:00</h2></header>
       <DP_Not_Answer />
       <CNR />
       <CDO />
