ce773ce1e9ce3f8d141e95f7a7e3a95cb03c68dc
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-04-30 09:21:52 +0530

    Update CNR.jsx

--- src/Components/CNR.jsx
+++ src/Components/CNR.jsx
@@ -38,19 +38,16 @@ const CNR = () => {
             <div className="space-y-4">
                 <div className="p-3 bg-gray-700 rounded-md shadow">
                     <ul className="list-disc pl-5">
-                        <li className="p-2">DP unable to call cx -- <strong className="text-yellow-300 font-semibold">Geo-Fence reached</strong> -- ETA+5 mins crossed -- asked to follow the automation process after ETA+5 mins.</li>
 
-                        <li className="p-2">DP unable to call cx -- <strong className="text-yellow-300 font-semibold">Geo-Fence reached</strong> -- ETA+5 mins crossed -- DP requested to call Cx -- call Cx -- <strong className='text-red-400 font-semibold'>Cx responsive</strong> -- same shared with rider.</li>
+                        <li className="p-2">DP unable to call cx -- <strong className="text-yellow-300 font-semibold">Geo-Fence reached</strong> -- ETA+5 mins crossed -- call Cx -- <strong className='text-red-400 font-semibold'>Cx responsive</strong> -- same shared with rider.</li>
 
-                        <li className="p-2">DP unable to call cx -- <strong className="text-yellow-300 font-semibold">Geo-Fence reached</strong> -- ETA+5 mins crossed -- DP requested to call Cx -- call Cx -- <strong className='text-red-400 font-semibold'>Cx not responsive</strong> -- asked to follow the automation process after ETA+5 mins.</li>
+                        <li className="p-2">DP unable to call cx -- <strong className="text-yellow-300 font-semibold">Geo-Fence reached</strong> -- ETA+5 mins crossed -- call Cx -- <strong className='text-red-400 font-semibold'>Cx not responsive</strong> -- asked to follow the automation process after ETA+5 mins.</li>
 
-                        <li className="p-2">DP unable to call cx -- <strong className="text-yellow-300 font-semibold">Geo-Fence reached</strong> -- ETA+5 mins crossed -- DP requested to call Cx -- call Cx -- <strong className='text-yellow-300 font-semibold'>Call forwarded to Voice-mail</strong> -- Order Cancel -- same shared with rider</li>
+                        <li className="p-2">DP unable to call cx -- <strong className="text-yellow-300 font-semibold">Geo-Fence reached</strong> -- ETA+5 mins crossed -- call Cx -- <strong className='text-yellow-300 font-semibold'>Call forwarded to Voice-mail</strong> -- Order Cancel -- same shared with rider</li>
 
                         <hr className='mb-5 mt-5' />
 
                         <li className="p-2">CNR -- <strong className="text-yellow-300 font-semibold">Geo-Fence reached</strong> -- ETA+5 mins crossed -- <strong className='text-yellow-300 font-semibold'>Call Attempt Not available ❎</strong> -- ask rider to make some call attempt and then follow automation in CNR </li>
-
-                        <li className="p-2">CNR -- <strong className="text-yellow-300 font-semibold">Geo-Fence reached</strong> -- ETA+5 mins crossed -- <strong className='text-yellow-300 font-semibold'>Call Attempt available ✅</strong> -- CNR attempt not available -- asked to follow the automation process.</li>
 
                         <li className="p-2">CNR -- <strong className="text-yellow-300 font-semibold">Geo-Fence reached</strong> -- ETA+5 mins crossed -- <strong className='text-yellow-300 font-semibold'>Call Attempt available ✅</strong> -- CNR attempt not available -- DP requested to call Cx -- call Cx -- <strong className='text-red-400 font-semibold'>Cx responsive</strong> -- shared the same with DP.</li>
 
