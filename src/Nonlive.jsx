6c4439d4f68e9457eeae7367f41f5991a5b477d0
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-05-07 13:03:50 +0530

    Add files via upload

---
+++ src/Nonlive.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Nonlive = () => {
+  return (
+    <div className='m-2 p-2 text-sm bg-gray-800 text-white rounded-md md:p-6 lg:p-8'>
+    <h2 className="text-center text-2xl font-semibold text-emerald-400 mb-2"> Non-live Order</h2>
+
+    <div className='space-y-4'>
+ 
+        <div className="p-3 bg-gray-700 rounded-md shadow">
+            <ul className="list-disc pl-5">
+
+                <li className='p-2'>Dp stated that bymistake order  marked delviered -- and wanted the cx details -- denied to share & requested to connect SM for better help - call close </li>
+
+                <li className='p-2'>Dp wanted to know about the extra pay -- explain & ask gto raise the ticket after delivery - call close </li>
+
+                <li className='p-2'>Dp said they don't had an issue now - call close </li>
+
+            </ul>
+        </div>
+ 
+
+    </div>
+
+</div>
+  )
+}
+
+export default Nonlive
