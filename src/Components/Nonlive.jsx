d06c4e2ff6ea324d1f1a12a409ad477ca2dbbc8a
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-05-11 13:12:09 +0530

    Update Nonlive.jsx

--- src/Components/Nonlive.jsx
+++ src/Components/Nonlive.jsx
@@ -9,12 +9,17 @@ const Nonlive = () => {
  
         <div className="p-3 bg-gray-700 rounded-md shadow">
             <ul className="list-disc pl-5">
+<li className='p-2'>Dp said they don't had an issue now - call close </li>
+<li className='p-2'>Dp wanted to know about the extra pay -- explain & ask to raise the ticket after delivery - call close </li>              
+<br></br> 
+<li className='p-2'>Dp stated that by mistake order marked delviered -- and wanted the cx details -- denied to share & requested to connect SM for better help - call close </li>
+<li className='p-2'> Non Live order related - batch order - wanted to know that they delivered the 2nd order first - ask to delivered the order in sequence which show in his app - call close</li>
+<br></br> 
+<li className='p-2'>-- call Cx -- Cx responsive -- cx wanted to cancel this order -- pushback given but not agreed to -- order canceled -- same share to rider  </li>               
 
-                <li className='p-2'>Dp stated that bymistake order  marked delviered -- and wanted the cx details -- denied to share & requested to connect SM for better help - call close </li>
-
-                <li className='p-2'>Dp wanted to know about the extra pay -- explain & ask gto raise the ticket after delivery - call close </li>
-
-                <li className='p-2'>Dp said they don't had an issue now - call close </li>
+                
+                
+             
 
             </ul>
         </div>
