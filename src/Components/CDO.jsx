d8d47a8258043aba3ad4b2a866287b26d5c7f234
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-06-08 21:56:51 +0530

    Update CDO.jsx

--- src/Components/CDO.jsx
+++ src/Components/CDO.jsx
@@ -4,84 +4,99 @@ const CDO = () => {
     return (
         <div className='m-2 p-4 text-sm bg-gray-800 text-white rounded-md md:p-6 lg:p-8'>
             <h2 className="text-center text-2xl font-semibold text-emerald-400 mb-1">CDO - Customer Denying the Order</h2>
-            
+
             <div className='space-y-4'>
-            <h3 className="text-lg font-bold">Before ETA + 10 mins</h3>
-            
-            <div className="p-3 bg-gray-700 rounded-md shadow">
-                <h3 className="text-md font-semibold text-emerald-300 mb-4">Cancel due to Mistake Reason:</h3>
-                <ul className="list-disc pl-5">
-                    <li className='p-2'>CDO -- ETA+10 mins not cross -- <strong className='text-yellow-300 font-semibold'>due to by mistake</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx ready to accept -- ask DP to ensure the delivery</li>
+                <h3 className="text-lg font-bold">Before ETA + 10 mins</h3>
 
-                    <li className='p-2'>CDO -- ETA+10 mins not cross -- <strong className='text-yellow-300 font-semibold'>due to by mistake</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx not ready to accept -- <strong className='text-yellow-300 font-semibold'>consent given by cx</strong> -- cancel order & refund TAT shared -- same info shared to DP</li>
+                <div className="p-3 bg-gray-700 rounded-md shadow">
+                    <h3 className="text-md font-semibold text-emerald-300 mb-4">Cancel due to Mistake Reason:</h3>
+                    <ul className="list-disc pl-5">
+                        <li className='p-2'>CDO -- ETA+10 mins not cross -- <strong className='text-yellow-300 font-semibold'>due to by mistake</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx ready to accept -- ask DP to ensure the delivery</li>
 
-                    <li className='p-2'>CDO -- ETA+10 mins not cross -- <strong className='text-yellow-300 font-semibold'>due to by mistake</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx not ready to accept -- <strong className='text-yellow-300 font-semibold'>consent not given by cx</strong> -- ask DP to follow automation in CDO</li>
-                </ul>
-            </div>
-            
-            <div className="p-3 bg-gray-700 rounded-md shadow">
-                <h3 className="text-md font-semibold text-emerald-300 mb-4">Cancel due to Delay Reason:</h3>
-                <ul className="list-disc pl-5">
-                    <li className='p-2'>CDO -- ETA+10 mins not cross -- <strong className='text-yellow-300 font-semibold'>due to Delay</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx ready to accept -- ask DP to ensure the delivery</li>
-                    <li className='p-2'>CDO -- ETA+10 mins not cross -- <strong className='text-yellow-300 font-semibold'>due to Delay</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx not ready to accept -- cancel the order & Refund TAT shared -- same info shared to DP</li>
-                    
-                </ul>
-            </div>
-            
-            <div className="p-3 bg-gray-700 rounded-md shadow">
-                <h3 className="text-md font-semibold text-emerald-300 mb-4">Guided to Chat Support</h3>
-                <ul className="list-disc pl-5">
-                <li className='p-2'>CDO -- ETA+10 mins not cross -- due to item missing/Expired/Rotten/Damage item/wrong item received -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- asked cx to connect their chat support -- same info shared to DP</li>
-                <li className='p-2'>CDO -- ETA+10 mins not cross -- due to item missing/Expired/Rotten/Damage item/wrong item received -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- asked cx to connect their chat support -- but cx insists us to cancel -- order canceled by me -- same info shared to DP</li>
-                </ul>
-            </div>
+                        <li className='p-2'>CDO -- ETA+10 mins not cross -- <strong className='text-yellow-300 font-semibold'>due to by mistake</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx not ready to accept -- <strong className='text-yellow-300 font-semibold'>consent given by cx</strong> -- cancel order & refund TAT shared -- same info shared to DP</li>
 
-            <div className="p-3 bg-gray-700 rounded-md shadow">
-                <h3 className="text-md font-semibold text-emerald-300 mb-4">Customer Not Responsive:</h3>
-                <ul className="list-disc pl-5">
-                    <li className='p-2'>CDO -- ETA+10 mins not cross -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- call recording available -- cx wants cancel -- order canceled by me -- same shared to DP</li>
-                    <li className='p-2'>CDO -- ETA+10 mins not cross -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- call recording available -- cx does not want to cancel -- ask DP to follow automation in CDO if cx wants to cancel else deliver the order</li>
-                    <li className='p-2'>CDO -- ETA+10 mins not cross -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- No call recording available -- ask DP to follow automation in CDO if cx wants to cancel else deliver the order</li>
-                </ul>
-            </div>
-            
-            <h3 className="text-lg font-bold">After ETA + 10 mins</h3>
-            
-            <div className="p-3 bg-gray-700 rounded-md shadow">
-                <h3 className="text-md font-semibold text-emerald-300 mb-4">Cancel due to Mistake Reason:</h3>
-                <ul className="list-disc pl-5">
-                    <li className='p-2'>CDO -- ETA+10 mins crossed -- <strong className='text-yellow-300 font-semibold'>due to by mistake</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx ready to accept -- ask DP to ensure the delivery</li>
+                        <li className='p-2'>CDO -- ETA+10 mins not cross -- <strong className='text-yellow-300 font-semibold'>due to by mistake</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx not ready to accept -- <strong className='text-yellow-300 font-semibold'>consent not given by cx</strong> -- ask DP to follow automation in CDO</li>
+                    </ul>
+                </div>
 
-                     <li className='p-2'>CDO -- ETA+10 mins crossed -- <strong className='text-yellow-300 font-semibold'>due to by mistake</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx not ready to accept -- <strong className='text-yellow-300 font-semibold'>consent given by cx</strong> -- cancel order & refund TAT shared -- same info shared to DP</li>
+                <div className="p-3 bg-gray-700 rounded-md shadow">
+                    <h3 className="text-md font-semibold text-emerald-300 mb-4">Cancel due to Delay Reason:</h3>
+                    <ul className="list-disc pl-5">
+                        <li className='p-2'>CDO -- ETA+10 mins not cross -- <strong className='text-yellow-300 font-semibold'>due to Delay</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx ready to accept -- ask DP to ensure the delivery</li>
+                        <li className='p-2'>CDO -- ETA+10 mins not cross -- <strong className='text-yellow-300 font-semibold'>due to Delay</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx not ready to accept -- cancel the order & Refund TAT shared -- same info shared to DP</li>
 
-                     <li className='p-2'>CDO -- ETA+10 mins crossed -- <strong className='text-yellow-300 font-semibold'>due to by mistake</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx not ready to accept -- <strong className='text-yellow-300 font-semibold'>consent not given by cx</strong> -- ask DP to follow automation in CDO</li>
-                </ul>
-            </div>
-            
-            <div className="p-3 bg-gray-700 rounded-md shadow">
-                <h3 className="text-md font-semibold text-emerald-300 mb-4">Cancel due to Delay Reason:</h3>
-                <ul className="list-disc pl-5">
-                    <li className='p-2'>CDO -- ETA+10 mins crossed -- <strong className='text-yellow-300 font-semibold'>due to Delay</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx ready to accept -- ask DP to ensure the delivery</li>
-                    <li className='p-2'>CDO -- ETA+10 mins crossed -- <strong className='text-yellow-300 font-semibold'>due to Delay</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx not ready to accept -- cancel the order & Refund TAT shared -- same info shared to DP</li>
-                </ul>
-            </div>
+                    </ul>
+                </div>
 
-            <div className="p-3 bg-gray-700 rounded-md shadow">
-                <h3 className="text-md font-semibold text-emerald-300 mb-4">Guided to Chat Support</h3>
-                <ul className="list-disc pl-5">
-                <li className='p-2'>CDO -- ETA+10 mins crossed -- due to item missing/Expired/Rotten/Damage item/wrong item received -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- asked cx to connect their chat support -- same info shared to DP</li>
-                <li className='p-2'>CDO -- ETA+10 mins crossed -- due to item missing/Expired/Rotten/Damage item/wrong item received -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- asked cx to connect their chat support -- but cx insists us to cancel -- order canceled by me -- same info shared to DP</li>
-                </ul>
-            </div>
+                <div className="p-3 bg-gray-700 rounded-md shadow">
+                    <h3 className="text-md font-semibold text-emerald-300 mb-4">Guided to Chat Support</h3>
+                    <ul className="list-disc pl-5">
+                        <li className='p-2'>CDO -- ETA+10 mins not cross -- due to item missing/Expired/Rotten/Damage item/wrong item received -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- asked cx to connect their chat support -- same info shared to DP</li>
+                        <li className='p-2'>CDO -- ETA+10 mins not cross -- due to item missing/Expired/Rotten/Damage item/wrong item received -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- asked cx to connect their chat support -- but cx insists us to cancel -- order canceled by me -- same info shared to DP</li>
+                    </ul>
+                </div>
 
-            <div className="p-3 bg-gray-700 rounded-md shadow">
-                <h3 className="text-md font-semibold text-emerald-300 mb-4">Customer Not Responsive:</h3>
-                <ul className="list-disc pl-5">
-                    <li className='p-2'>CDO -- ETA+10 mins cross -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- call recording available -- cx wants cancel -- order canceled by me -- same shared to DP</li>
-                    <li className='p-2'>CDO -- ETA+10 mins cross -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- call recording available -- cx does not want to cancel -- ask DP to follow automation in CDO if cx wants to cancel else deliver the order</li>
-                    <li className='p-2'>CDO -- ETA+10 mins cross -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- No call recording available -- ask DP to follow automation in CDO if cx wants to cancel else deliver the order</li>
-                </ul>
-            </div>
+                <div className="p-3 bg-gray-700 rounded-md shadow">
+                    <h3 className="text-md font-semibold text-emerald-300 mb-4">Customer Not Responsive:</h3>
+                    <ul className="list-disc pl-5">
+                        <li className='p-2'>CDO -- ETA+10 mins not cross -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- call recording available -- cx wants cancel -- order canceled by me -- same shared to DP</li>
+                        <li className='p-2'>CDO -- ETA+10 mins not cross -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- call recording available -- cx does not want to cancel -- ask DP to follow automation in CDO if cx wants to cancel else deliver the order</li>
+                        <li className='p-2'>CDO -- ETA+10 mins not cross -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- No call recording available -- ask DP to follow automation in CDO if cx wants to cancel else deliver the order</li>
+
+                        <li className='p-2'>CDO -- ETA+10 mins not cross -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- Error in OS in call recording -- ask DP to follow automation in CDO if cx wants to cancel else deliver the order</li>
+                    </ul>
+                </div>
+
+                <h3 className="text-lg font-bold">After ETA + 10 mins</h3>
+
+                <div className="p-3 bg-gray-700 rounded-md shadow">
+                    <h3 className="text-md font-semibold text-emerald-300 mb-4">Cancel due to Mistake Reason:</h3>
+                    <ul className="list-disc pl-5">
+                        <li className='p-2'>CDO -- ETA+10 mins crossed -- <strong className='text-yellow-300 font-semibold'>due to by mistake</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx ready to accept -- ask DP to ensure the delivery</li>
+
+                        <li className='p-2'>CDO -- ETA+10 mins crossed -- <strong className='text-yellow-300 font-semibold'>due to by mistake</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx not ready to accept -- <strong className='text-yellow-300 font-semibold'>consent given by cx</strong> -- cancel order & refund TAT shared -- same info shared to DP</li>
+
+                        <li className='p-2'>CDO -- ETA+10 mins crossed -- <strong className='text-yellow-300 font-semibold'>due to by mistake</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx not ready to accept -- <strong className='text-yellow-300 font-semibold'>consent not given by cx</strong> -- ask DP to follow automation in CDO</li>
+                    </ul>
+                </div>
+
+                <div className="p-3 bg-gray-700 rounded-md shadow">
+                    <h3 className="text-md font-semibold text-emerald-300 mb-4">Cancel due to Delay Reason:</h3>
+                    <ul className="list-disc pl-5">
+                        <li className='p-2'>CDO -- ETA+10 mins crossed -- <strong className='text-yellow-300 font-semibold'>due to Delay</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx ready to accept -- ask DP to ensure the delivery</li>
+                        <li className='p-2'>CDO -- ETA+10 mins crossed -- <strong className='text-yellow-300 font-semibold'>due to Delay</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx not ready to accept -- cancel the order & Refund TAT shared -- same info shared to DP</li>
+                    </ul>
+                </div>
+
+                <div className="p-3 bg-gray-700 rounded-md shadow">
+                    <h3 className="text-md font-semibold text-emerald-300 mb-4">Guided to Chat Support</h3>
+                    <ul className="list-disc pl-5">
+                        <li className='p-2'>CDO -- ETA+10 mins crossed -- due to item missing/Expired/Rotten/Damage item/wrong item received -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- asked cx to connect their chat support -- same info shared to DP</li>
+                        <li className='p-2'>CDO -- ETA+10 mins crossed -- due to item missing/Expired/Rotten/Damage item/wrong item received -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- asked cx to connect their chat support -- but cx insists us to cancel -- order canceled by me -- same info shared to DP</li>
+                    </ul>
+                </div>
+
+                <div className="p-3 bg-gray-700 rounded-md shadow">
+                    <h3 className="text-md font-semibold text-emerald-300 mb-4">Customer Not Responsive:</h3>
+                    <ul className="list-disc pl-5">
+                        <li className='p-2'>CDO -- ETA+10 mins cross -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- call recording available -- cx wants cancel -- order canceled by me -- same shared to DP</li>
+                        <li className='p-2'>CDO -- ETA+10 mins cross -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- call recording available -- cx does not want to cancel -- ask DP to follow automation in CDO if cx wants to cancel else deliver the order</li>
+                        <li className='p-2'>CDO -- ETA+10 mins cross -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- No call recording available -- ask DP to follow automation in CDO if cx wants to cancel else deliver the order</li>
+                    </ul>
+                </div>
+
+                <div className="p-3 bg-gray-700 rounded-md shadow">
+                    <h3 className="text-md font-semibold text-emerald-300 mb-4">Customer Not Responsive:</h3>
+                    <ul className="list-disc pl-5">
+                        <li className='p-2'>CDO -- ETA+10 mins cross -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- call recording available -- cx wants cancel -- order canceled by me -- same shared to DP</li>
+
+                        <li className='p-2'>CDO -- ETA+10 mins cross -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- call recording available -- cx does not want to cancel -- ask DP to follow automation in CDO if cx wants to cancel else deliver the order</li>
+
+                        <li className='p-2'>CDO -- ETA+10 mins cross -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- No call recording available -- ask DP to follow automation in CDO if cx wants to cancel else deliver the order</li>
+
+                         <li className='p-2'>CDO -- ETA+10 mins cross -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- Error in OS in call recording -- ask DP to follow automation in CDO if cx wants to cancel else deliver the order</li>
+                    </ul>
+                </div>
             </div>
 
         </div>
