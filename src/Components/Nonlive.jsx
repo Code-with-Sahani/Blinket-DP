ffb7adf04a9c422d2418e788b117a21606c8e46a
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-07-25 20:48:10 +0530

    Update Nonlive.jsx

--- src/Components/Nonlive.jsx
+++ src/Components/Nonlive.jsx
@@ -2,32 +2,28 @@ import React from 'react'
 
 const Nonlive = () => {
   return (
-    <div className='m-2 p-2 text-sm bg-gray-800 text-white rounded-md md:p-6 lg:p-8'>
-    <h2 className="text-center text-2xl font-semibold text-emerald-400 mb-2"> Non-live Order</h2>
+    <div className='m-2 p-2 text-sm bg-gray-400 text-white rounded-md md:p-6 lg:p-8'>
+      <h2 className="text-center text-2xl font-bold text-black mb-8"> Non-live Order</h2>
 
-    <div className='space-y-4'>
- 
-        <div className="p-3 bg-gray-700 rounded-md shadow">
-            <ul className="list-disc pl-5">
-<li className='p-2'>Dp said they don't had an issue now - call close </li>
-<li className='p-2'>Dp wanted to know about the extra pay -- explain & ask to raise the ticket after delivery - call close </li>              
-<br></br> 
-<li className='p-2'>Dp stated that by mistake order marked delviered -- and wanted the cx details -- denied to share & requested to connect SM for better help - call close </li>
-<li className='p-2'> Non Live order related - batch order - wanted to know that they delivered the 2nd order first - ask to delivered the order in sequence which show in his app - call close</li>
-<br></br> 
-<li className='p-2'>-- call Cx -- Cx responsive -- cx wanted to cancel this order -- pushback given but not agreed to -- order canceled -- same share to rider  </li>               
+      <div className='space-y-4'>
 
-                
-                
-             
+        <div className="p-3 bg-gradient-to-b from-gray-800 to-gray-700 text-green-300 rounded-md shadow">
+          <ul className="list-disc pl-5">
+            <li className='p-2'>Dp said they don't had an issue now - call close </li>
+            <li className='p-2'>Dp wanted to know about the extra pay -- explain & ask to raise the ticket after delivery - call close </li>
+            <br></br>
+            <li className='p-2'>Dp stated that by mistake order marked delviered -- and wanted the cx details -- denied to share & requested to connect SM for better help - call close </li>
+            <li className='p-2'> Non Live order related - batch order - wanted to know that they delivered the 2nd order first - ask to delivered the order in sequence which show in his app - call close</li>
+            <br></br>
+            <li className='p-2'>-- call Cx -- Cx responsive -- cx wanted to cancel this order -- pushback given but not agreed to -- order canceled -- same share to rider  </li>
 
-            </ul>
+          </ul>
         </div>
- 
+
+
+      </div>
 
     </div>
-
-</div>
   )
 }
 
