a9cf84e4f539dfc419e03b4357f6359f85219e02
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-07-25 20:28:02 +0530

    Update CDO.jsx

--- src/Components/CDO.jsx
+++ src/Components/CDO.jsx
@@ -2,26 +2,64 @@ import React from 'react';
 
 const CDO = () => {
     return (
-        <div className='m-2 p-4 text-sm bg-gray-800 text-white rounded-md md:p-6 lg:p-8'>
-            <h2 className="text-center text-2xl font-semibold text-emerald-400 mb-1">CDO - Customer Denying the Order</h2>
+        <div className='m-2 p-4 text-sm bg-gray-200 text-white rounded-md md:p-6 lg:p-8'>
+            <h2 className="text-center text-2xl font-semibold text-emerald-400 mb-4">CDO - Customer Denying the Order</h2>
 
             <div className='space-y-4'>
-              
 
-                <div className="p-3 bg-gray-700 rounded-md shadow">
+                <div className="p-3 bg-gradient-to-b from-gray-800 to-gray-700 text-green-300 rounded-md shadow">
+                    <h3 className="text-md font-semibold text-emerald-300 mb-4">CDO : Due to Address Issue Updates</h3>
+                    <ul className="list-disc pl-5">
+                        <li className='p-2'>CDO -- Address wrong -- Address given by cx -- <strong className='text-green-400'>Serviceable
+                        </strong> -- ask rider to deliver at cx correct address -- ask to raise ticket as well after delivery for extra payout -- rider agree -- Call close
+                        </li>
+
+                        <li className='p-2'>CDO -- Address wrong -- Address given by cx -- <strong className='text-green-400'>Serviceable
+                        </strong> -- ask rider to deliver at cx correct address -- rider denied and adamant -- order cancel by informing the cx -- same share to rider</li>
+
+                        <li className='p-2'>CDO -- Address wrong -- Address given by cx -- <strong className='text-green-400'>Serviceable
+                        </strong> -- ask rider to deliver at cx correct address -- rider denied and adamant -- order cancel -- refund TAT sahre to cx -- same share to rider</li>
+                    </ul>
+                    <br />
+
+                    <h3 className="text-md font-semibold text-emerald-300 mb-4 mt-4">CDO : Address wrong // Other State or District </h3>
+                    <ul className="list-disc pl-5">
+                        <li className='p-2'>CDO -- Address wrong -- Address given by cx -- <strong className='text-green-400'>Not Serviceable
+                        </strong> -- other state/District -- order cancel by informing the cx -- same share to rider</li>
+
+                        <li className='p-2'>CDO -- Address wrong -- Address given by cx -- <strong className='text-green-400'>Not Serviceable
+                        </strong> -- other state/District -- order cancel -- order cancel -- refund TAT sahre to cx -- same share to rider</li>
+
+                        <hr className=' mt-8 mb-8' />
+
+                        <li className='p-2'>CDO -- Address wrong -- Address given by cx -- <strong className='text-green-400'>Not Serviceable
+                        </strong> -- ask rider to deliver at cx correct address -- rider agree with the extra payout -- ask to raise a ticket -- Call close
+                        </li>
+
+                        <li className='p-2'>CDO -- Address wrong -- Address given by cx -- <strong className='text-green-400'>Not Serviceable
+                        </strong> -- ask rider to deliver at cx correct address -- rider disagree with the extra payout -- order cancel by informing cx -- same share to rider </li>
+
+                        <li className='p-2'>CDO -- Address wrong -- Address given by cx -- <strong className='text-green-400'>Not Serviceable
+                        </strong> -- ask rider to deliver at cx correct address -- rider disagree with the extra payout -- order cancel -- refund TAT sahre to cx -- same share to rider</li>
+
+
+                    </ul>
+                </div>
+
+                <div className="p-3 bg-gradient-to-b from-gray-800 to-gray-700 text-green-300 rounded-md shadow">
                     <h3 className="text-md font-semibold text-emerald-300 mb-4">Cancel due to Mistake Reason:</h3>
                     <ul className="list-disc pl-5">
                         <li className='p-2'>CDO -- <strong className='text-yellow-300 font-semibold'>due to by mistake</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx ready to accept -- ask DP to ensure the delivery</li>
 
-                         <li className='p-2'>CDO -- <strong className='text-yellow-300 font-semibold'>due to by mistake</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx not ready to accept -- <strong className='text-yellow-300 font-semibold'>consent given by cx</strong> -- cancel order -- same info shared to DP</li>
+                        <li className='p-2'>CDO -- <strong className='text-yellow-300 font-semibold'>due to by mistake</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx not ready to accept -- <strong className='text-yellow-300 font-semibold'>consent given by cx</strong> -- cancel order -- same info shared to DP</li>
 
                         <li className='p-2'>CDO -- <strong className='text-yellow-300 font-semibold'>due to by mistake</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx not ready to accept -- <strong className='text-yellow-300 font-semibold'>consent given by cx</strong> -- cancel order & refund TAT shared -- same info shared to DP</li>
- 
+
                         <li className='p-2'>CDO -- <strong className='text-yellow-300 font-semibold'>due to by mistake</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx not ready to accept -- <strong className='text-yellow-300 font-semibold'>consent not given by cx</strong> -- ask DP to follow automation in CDO</li>
                     </ul>
                 </div>
 
-                <div className="p-3 bg-gray-700 rounded-md shadow">
+                <div className="p-3 bg-gradient-to-b from-gray-800 to-gray-700 text-green-300 rounded-md shadow">
                     <h3 className="text-md font-semibold text-emerald-300 mb-4">Cancel due to Delay Reason:</h3>
                     <ul className="list-disc pl-5">
                         <li className='p-2'>CDO -- <strong className='text-yellow-300 font-semibold'>due to Delay</strong> -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- tried to convince customer -- cx ready to accept -- ask DP to ensure the delivery</li>
@@ -30,7 +68,7 @@ const CDO = () => {
                     </ul>
                 </div>
 
-                <div className="p-3 bg-gray-700 rounded-md shadow">
+                <div className="p-3 bg-gradient-to-b from-gray-800 to-gray-700 text-green-300 rounded-md shadow">
                     <h3 className="text-md font-semibold text-emerald-300 mb-4">Guided to Chat Support</h3>
                     <ul className="list-disc pl-5">
                         <li className='p-2'>CDO -- due to item missing/Expired/Rotten/Damage item/wrong item received -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- asked cx to connect their chat support -- same info shared to DP</li>
@@ -38,7 +76,7 @@ const CDO = () => {
                     </ul>
                 </div>
 
-                <div className="p-3 bg-gray-700 rounded-md shadow">
+                <div className="p-3 bg-gradient-to-b from-gray-800 to-gray-700 text-green-300 rounded-md shadow">
                     <h3 className="text-md font-semibold text-emerald-300 mb-4">Customer Not Responsive:</h3>
                     <ul className="list-disc pl-5">
                         <li className='p-2'>CDO -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- call recording available -- cx wants cancel -- order canceled by me -- same shared to DP</li>
@@ -48,7 +86,11 @@ const CDO = () => {
                         <li className='p-2'>CDO -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- Error in OS in call recording -- ask DP to follow automation in CDO if cx wants to cancel else deliver the order</li>
                     </ul>
                 </div>
+
+                
             </div>
+
+
 
         </div>
     );
