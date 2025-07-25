23e150ca3e8bc73cc8bf2d57fa336b1e7955417e
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-07-25 20:45:27 +0530

    Update DP_Not_Answer.jsx

--- src/Components/DP_Not_Answer.jsx
+++ src/Components/DP_Not_Answer.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const DP_Not_Answer = () => {
   return (
-    <div className='m-2 p-4 text-sm bg-gray-800 text-white rounded-md md:p-6 lg:p-8'>
-    <h2 className="text-center text-2xl font-semibold text-emerald-400 mb-1"> DP Not Answering </h2>
+    <div className='m-2 p-4 text-sm bg-gray-400 text-white rounded-md md:p-6 lg:p-8'>
+    <h2 className="text-center text-2xl font-bold text-black mb-8"> DP Not Answering </h2>
 
     <div className='space-y-4'>
  
-        <div className="p-3 bg-gray-700 rounded-md shadow">
+        <div className="p-3  bg-gradient-to-b from-gray-800 to-gray-700 text-green-300 rounded-md shadow">
             <ul className="list-disc pl-5">
 
                 <li className='p-2'>Dp Not Responding -- Call Twice -- Not responsive </li>
