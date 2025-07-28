099a28ad85b8165fe3828c08ab5aff1fa3b25000
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-07-28 19:26:48 +0530

    Update Nonlive.jsx

--- src/Components/Nonlive.jsx
+++ src/Components/Nonlive.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 const Nonlive = () => {
   return (
-    <div className='m-2 p-2 text-sm bg-gray-400 text-white rounded-md md:p-6 lg:p-8'>
-      <h2 className="text-center text-2xl font-bold text-black mb-8"> Non-live Order</h2>
+    <div className='m-2 p-2 text-sm bg-gray-800 text-white rounded-md md:p-6 lg:p-8'>
+      <h2 className="text-center text-2xl font-bold text-emerald-400 mb-8"> Non-live Order</h2>
 
       <div className='space-y-4'>
 
-        <div className="p-3 bg-gradient-to-b from-gray-800 to-gray-700 text-green-300 rounded-md shadow">
+        <div className="p-3 bg-gray-700 rounded-md shadow">
           <ul className="list-disc pl-5">
             <li className='p-2'>Dp said they don't had an issue now - call close </li>
+            <li className='p-2'>Non live order - cant able to coordinate with DP  - language issues - call close </li>
             <li className='p-2'>Dp wanted to know about the extra pay -- explain & ask to raise the ticket after delivery - call close </li>
             <br></br>
             <li className='p-2'>Dp stated that by mistake order marked delviered -- and wanted the cx details -- denied to share & requested to connect SM for better help - call close </li>
