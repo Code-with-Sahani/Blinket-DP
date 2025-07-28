2c9e994159d96af5ad6ae67670c22a4ee3889280
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-07-28 19:42:15 +0530

    Update Large_Order.jsx

--- src/Components/Large_Order.jsx
+++ src/Components/Large_Order.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 const Large_Order = () => {
   return (
-    <div className='m-2 p-2 text-sm bg-gray-400 text-white rounded-md md:p-6 lg:p-8'>
-      <h2 className="text-center text-2xl font-bold text-black mb-8">Large Order</h2>
+    <div className='m-2 p-2 text-sm bg-gray-800 text-white rounded-md md:p-6 lg:p-8'>
+      <h2 className="text-center text-2xl font-semibold text-emerald-400 mb-8">Large Order</h2>
       <div className='space-y-4'>
-        <div className="p-3 bg-gradient-to-b from-gray-800 to-gray-700 text-green-300 rounded-md shadow">
+        <div className="p-3 bg-gray-700 rounded-md shadow">
           <ul className="list-disc pl-5">
             <li className='p-2'>CDO -- by mistake/address issue/Duplicate order -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- Push back given to Cx -- cx denied to accept -- ask rider to Submit the order at store -- then reconnect us again regarding the cancellation.</li>
             <li className='p-2'>CDO -- by mistake/address issue/Duplicate order -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- Push back given to Cx -- cx denied to accept -- order submitted at store -- called SM -- SM confirmed -- highlighted to concern team for cancellation  -- same shared to DP</li>
+            <li className='p-2'>CDO -- by mistake/address issue/Duplicate order -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- Push back given to Cx -- cx denied to accept -- order submitted at store -- called SM -- SM unresponsive -- rider at store according to the map -- highlighted to concern team for cancellation  -- same shared to DP</li> 
             <li className='p-2'>CDO -- by mistake/address issue/Duplicate order -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- Push back given to Cx -- cx denied to accept -- order submitted at store -- called SM -- SM unresponsive -- Order cannot be canceled without SM confirmation -- Same shared to DP</li>
              
           </ul>
