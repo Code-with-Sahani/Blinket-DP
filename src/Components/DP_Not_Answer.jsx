5a7024dc89272c3556d225f4dc095ec36d0dd742
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-08-09 11:55:45 +0530

    Update DP_Not_Answer.jsx

--- src/Components/DP_Not_Answer.jsx
+++ src/Components/DP_Not_Answer.jsx
@@ -10,8 +10,32 @@ const DP_Not_Answer = () => {
         <div className="p-3 bg-gray-700  rounded-md shadow">
             <ul className="list-disc pl-5">
 
-               <li className='p-2'>Call Received by me -- but DP disconnect the call himself without saying anythings</li>
-                <li className='p-2'>Dp Not Responding -- Call once -- Not responsive </li>
+               
+<div className="mb-8 border border-yellow-400">
+                <h3 className="text-xl font-semibold text-yellow-400 mb-2 mt-4 pb-2">
+                    🚚 Only for Today
+                </h3>
+                <ul className="list-disc pl-5 pr-5 list-inside space-y-4 text-sm leading-relaxed">
+                   <li className='p-2'>Call Received by me -- but DP disconnect the call himself without saying anythings</li>
+                   
+                  <li className='p-2'>Dp Not Responding -- Call once -- Not responsive </li>
+                    <li >
+                        Address issue -- location mismatch -- Distance between store & address is more than 4km -- rider agreed with extra pay -- ask rider to deliver
+                    </li>
+
+                   <li >
+                        Address issue -- location mismatch -- Distance between store & address is more than 4km -- rider disagreed with extra pay -- order cancel & refund TAT share to cx -- same sahre to rider 
+                    </li>
+
+                   <li >
+                        Address issue -- location mismatch -- Distance between store & address is more than 4km -- rider disagreed with extra pay -- Cx not responsive -- order cancel -- same sahre to rider 
+                    </li>
+ 
+                      
+
+                    </div>
+
+              
               <li className='p-2'>Dp Not Responding -- Call Twice -- Not responsive </li>
                 <li className='p-2'>Dp Not Responding -- previous Agent already Call him -- hence Call Once -- Not Answering</li>
 
@@ -26,5 +50,6 @@ const DP_Not_Answer = () => {
 }
 
 export default DP_Not_Answer
+
 
 
