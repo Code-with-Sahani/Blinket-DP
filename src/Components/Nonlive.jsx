6dd8def7e9ba65f8a1830370f864cb01dd639bf0
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-10-15 19:09:40 +0530

    Update Nonlive.jsx

--- src/Components/Nonlive.jsx
+++ src/Components/Nonlive.jsx
@@ -18,10 +18,10 @@ const Nonlive = () => {
             <li className='p-2'> Non Live order related - batch order - wanted to know that they delivered the 2nd order first - ask to delivered the order in sequence which show in his app - call close</li>
             <br></br>
 
-            <li className='p-2'>-- call Cx -- Cx responsive -- cx wanted to cancel this order -- pushback given but not agreed to -- order canceled -- same share to rider  </li>
-            <br></br>
              <li className='p-2'>Non Live order related -- cx said he/she did come in the next x min but till now not came yet -- ETA+ 12 min not cross -- called cx -- cx not responsive -- call Cx -- Cx Not responsive -- ask rider to follow automation till ETA +12 min along with some call attempt </li>
              <li className='p-2'>Non Live order related -- cx said he/she did come in the next x min but till now not came yet -- ETA+ 12 min not cross -- called cx -- cx responsive -- cx said her agent reached at gate in the next 2 min  - same share to rider  & ask to ensure delivery</li>
+          <li className='p-2'>-- call Cx -- Cx responsive -- cx wanted to cancel this order -- pushback given but not agreed to -- order canceled -- same share to rider  </li>
+            <br></br>
           </ul>
         </div>
 
@@ -33,4 +33,5 @@ const Nonlive = () => {
 }
 
 export default Nonlive
+
 
