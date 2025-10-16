2f1b259d01dd9c3407f8e66c5cdc5c8a534e8594
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-10-16 21:33:37 +0530

    Update BuddyRider.jsx

--- src/Components/BuddyRider.jsx
+++ src/Components/BuddyRider.jsx
@@ -5,6 +5,12 @@ const BuddyRider = () => {
         <div className='m-2 p-4 text-sm bg-gray-800 text-white rounded-md md:p-6 lg:p-8'>
             <h2 className="text-center text-2xl font-semibold text-emerald-400 mb-1">Buddy Rider Assignment</h2>
 
+             <p className="text-sm font-serif border border-green-400 mb-2 mt-4 p-2">
+                <h3 className="text-center font-semibold text-red-400 mb-1"> Update only 16-22 OCT 2025 </h3>
+                 <br></br>
+                 <li className='p-2'>DP requested BR -- 2-wheeler -- <strong className='text-yellow-300 font-semibold'> 9 Kg</strong> -- enroute -- No Buddy assigned -- hence assigned 1 BR by me -- share same to DP</li> 
+            </p>
+
             <div className='space-y-4'>
 
                 
@@ -24,8 +30,7 @@ const BuddyRider = () => {
                     <h3 className="text-md font-semibold text-emerald-300 mb-4">Order weight less than 20 Kg :</h3>
                     <ul className="list-disc pl-5">
                          <li className='p-2'>DP requested BR -- 2-wheeler -- <strong className='text-yellow-300 font-semibold'>18 Kg</strong> -- tried to convince to deliver itself -- DP convinced -- asked to delivered as soon as possible</li>
-                         <li className='p-2'>DP requested BR -- 2-wheeler -- <strong className='text-yellow-300 font-semibold'> __ Kg</strong> -- enroute -- No Buddy assigned -- hence assigned 1 BR by me -- share same to DP</li>
-                        
+                         
                          <li className='p-2'>DP requested BR -- 2-wheeler -- tried to convince to deliver itself -- not convinced -- highlight to concern team -- asked DP to wait for call back from concern team</li>
 
                     </ul>
@@ -71,5 +76,6 @@ const BuddyRider = () => {
 }
 
 export default BuddyRider
+
 
 
