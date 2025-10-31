ebbd88a06bf173aaede4d1b5fab84d17ada0b473
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-10-31 22:07:20 +0530

    Update PaymentIssue.jsx

--- src/Components/PaymentIssue.jsx
+++ src/Components/PaymentIssue.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PaymentIssue = () => {
     return (
-        <div className="m-2 p-4 text-sm font-serif bg-gray-800 text-white rounded-md md:p-6 lg:p-8">
+        <div className="m-2 p-4 text-sm   bg-gray-800 text-white rounded-md md:p-6 lg:p-8">
             <h2 className='text-center text-2xl font-semibold text-emerald-400 mb-1'>PAYMENT ISSUE</h2>
             <h3 className="text-lg font-semibold text-amber-400 mb-3">Inactive Showing -- Cx trying to Pay</h3>
 
@@ -21,13 +21,13 @@ const PaymentIssue = () => {
 
                 <div className="p-3 bg-gray-700 rounded-md shadow">
                 <div className="text-sm border border-green-400 mb-2 mt-4 p-2">
-                       <li className="text-sm font-serif mb-2 mt-4 p-2">
+                       <li className="text-sm   mb-2 mt-4 p-2">
               Payment Issue -- check payment status -- pending -- rider not getting an option of "Get help" i.e. rider not able to follow new update of payment -- so guided SM for better help -- call close 
             </li>
-                    <li className="text-sm font-serif 0 mb-2 mt-4 p-2">
+                    <li className="text-sm   0 mb-2 mt-4 p-2">
               Payment Issue -- check payment status -- pending -- show "Something went wrong" when rider upload payment proof -- ask rider to refresh this but stil same -- guided to SM for better help -- call close 
             </li>
-                    <li className="text-sm font-serif mb-2 mt-4 p-2">
+                    <li className="text-sm   mb-2 mt-4 p-2">
               Payment Issue -- check payment status -- pending -- asked rider to Click on "Get help" -- and then upload cx's payment proof -- then ask to click "cancelling QR payment" -- ask rider to follow this step bcos after this cx get notified regarding his refund -- cx get ready to do payment again -- if not doing payment again by cx -- then reconnect us again (rider nto able to follow this steps over the call)
             </li>
             
@@ -78,6 +78,7 @@ const PaymentIssue = () => {
 }
 
 export default PaymentIssue;
+
 
 
 
