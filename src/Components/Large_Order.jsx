38a2f81c8081bba5b54e9475816d7dfba8756535
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-11-11 01:28:21 +0530

    Update Large_Order.jsx

--- src/Components/Large_Order.jsx
+++ src/Components/Large_Order.jsx
@@ -8,9 +8,9 @@ const Large_Order = () => {
         <div className="p-3 bg-gray-700 rounded-md shadow">
             <div className="text-sm border border-green-400 mb-4 mt-2 p-2">
               
-          <p className="text-sm mb-2 mt-4 p-2">Apple Order-CDO -- by mistake/Duplicate order -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- cx denied to accept -- ask rider to Submit the order at store -- then reconnect us again regarding the cancellation.</p>
-          <p className="text-sm mb-2 mt-4 p-2">Apple Order-CDO -- by mistake/Duplicate order -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- cx denied to accept -- order submitted at store -- called SM -- SM confirmed -- highlighted to concern team for cancellation -- same shared to DP</p>
-          <p className="text-sm mb-2 mt-4 p-2">Apple Order-CDO -- by mistake/Duplicate order -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- cx denied to accept -- order submitted at store -- called SM -- SM unresponsive -- rider at store according to the map -- highlighted to concern team for cancellation -- same shared to DP</p>
+          <p className="text-sm mb-2 mt-2 p-2">Apple Order-CDO -- by mistake/Duplicate order -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- cx denied to accept -- ask rider to Submit the order at store -- then reconnect us again regarding the cancellation.</p>
+          <p className="text-sm mb-2 mt-2 p-2">Apple Order-CDO -- by mistake/Duplicate order -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- cx denied to accept -- order submitted at store -- called SM -- SM confirmed -- highlighted to concern team for cancellation -- same shared to DP</p>
+          <p className="text-sm mb-2 mt-2 p-2">Apple Order-CDO -- by mistake/Duplicate order -- called cx -- <strong className='text-red-400'>cx responsive</strong> -- cx denied to accept -- order submitted at store -- called SM -- SM unresponsive -- rider at store according to the map -- highlighted to concern team for cancellation -- same shared to DP</p>
             </div>
           
           <ul className="list-disc pl-5">
@@ -25,5 +25,6 @@ const Large_Order = () => {
 }
 
 export default Large_Order
+
 
 
