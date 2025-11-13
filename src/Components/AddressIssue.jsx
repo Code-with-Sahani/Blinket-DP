c02bb500005f3eca61602a99025d4d9feecbd806
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-11-13 12:39:52 +0530

    Update AddressIssue.jsx

--- src/Components/AddressIssue.jsx
+++ src/Components/AddressIssue.jsx
@@ -6,12 +6,16 @@ const AddressIssue = () => {
             <h2 className='text-center text-2xl font-semibold text-emerald-400 mb-4'>Address Issue </h2>
 
             <div className="text-sm border border-green-400 mb-2 mt-4 p-2">
-                <p className="text-sm mb-2 mt-4 p-2">
+                {/* <p className="text-sm mb-2 mt-4 p-2">
                     Address issue -- mentioning distance here --  rider wanted to know where he has to go -- called cx -- ask cx to call rider for the correct location guidance -- same share to rider
                 </p>
 
                 <p className="text-sm mb-2 mt-4 p-2">
                     Address issue -- mentioning distance here --  rider wanted to know where he has to go -- called cx -- cx not responsive -- ask rider to reach at given cx's location -- ask rider to follow the automation process & guide the steps
+                </p> */}
+
+                <p className="text-sm mb-2 mt-4 p-2">
+                     Address issue// location issue -- but rider is not at geo-fence or at location -- ask rider to reach the geo-fence or at location then coordinate with cx and then deliver the order --if any then reconnect us again
                 </p>
 
                 <p className="text-sm mb-2 mt-4 p-2">
@@ -58,7 +62,7 @@ const AddressIssue = () => {
                     <ul className="list-disc pl-5">
                         <li className="p-2">Address Issue -- Distance between <strong className="text-yellow-400 font-semibold"> location & address </strong>  is less than 400 m -- ask rider to deliver the order -- raise the ticket for the extra distance if travelled</li>
 
-                        <li className="p-2">Address Issue -- Distance between <strong className="text-yellow-400 font-semibold"> location & address </strong>  is less than 400 m --  ask rider to reach the location then coordinate with cx and then deliver the order -- raise the ticket for the extra distance if travelled</li>
+                        <li className="p-2">Address Issue -- Distance between <strong className="text-yellow-400 font-semibold"> location & address </strong>  is less than 400 m -- ask rider to reach the location then coordinate with cx and then deliver the order -- raise the ticket for the extra distance if travelled</li>
 
                         <li className="p-2">Address Issue -- Distance between <strong className="text-yellow-400 font-semibold"> location & address </strong>  is less than 400 m --  called cx -- ask cx to call rider for the better location guidance -- ask rider to deliver the order -- raise the ticket for the extra distance if travelled</li>
 
@@ -100,6 +104,7 @@ const AddressIssue = () => {
 };
 
 export default AddressIssue;
+
 
 
 
