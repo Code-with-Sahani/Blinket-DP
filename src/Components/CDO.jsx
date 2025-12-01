6c3c6719c4594f3a67e2d773a8678bbbc08934e8
Rahul Sahani <111352713+Code-with-Sahani@users.noreply.github.com>
2025-12-01 13:38:56 +0530

    Update CDO.jsx

--- src/Components/CDO.jsx
+++ src/Components/CDO.jsx
@@ -9,41 +9,28 @@ const CDO = () => {
 
                 <div className="p-3 bg-gray-900 rounded-md shadow">
                     <h3 className="text-md font-semibold text-emerald-300 mb-4">CDO : Due to Address Issue Updates</h3>
-                    <ul className="list-disc pl-5">
-                        <li className='p-2'>CDO -- Address wrong -- Address given by cx -- <strong className='text-green-400'>Mention Distance here -- Serviceable
-                        </strong> -- ask rider to deliver at cx correct address -- ask to raise ticket as well after delivery for extra payout -- rider agree -- Call close</li>
+                    <div className="list-disc pl-5">
+                        <p className='p-2'>CDO -- Address wrong -- Address given by cx -- as seen in recent address -- cx had previously received order at that address -- ask rider to deliver at cx correct address -- ask to raise ticket as well after delivery for extra payout -- Call close</p>
 
-                        <li className='p-2'>CDO -- Address wrong -- Address given by cx -- <strong className='text-green-400'>Mention Distance here -- Serviceable
-                        </strong> -- Rider adamant and not ready to go at address & agree to beer penalty -- order cancel by informing cx -- same share to rider</li>
+                        <p className='p-2'>CDO -- Address wrong -- Address given by cx -- as seen in recent address -- cx had not previously received order at that address -- after onward verify all the distances -- and found that the cx given address was serviceable -- ask rider to deliver & ask to raise ticket for extra payout -- Call close</p>
 
-                       <li className='p-2'>CDO -- Address wrong -- Address given by cx -- <strong className='text-green-400'>Mention Distance here -- Serviceable
-                        </strong> -- Rider adamant and not ready to go at address & agree to beer penalty -- order cancel by informing cx -- refund TAT shared as well to cx -- same share to rider</li>
+                        <p className='p-2'>CDO -- Address wrong -- Address given by cx -- as seen in recent address -- cx had not previously received order at that address  -- more than 500 m & also more than 4 km -- Not Serviceable -- ask rider to deliver at cx correct address -- rider agree with the extra payout -- ask to raise a ticket -- Call close</p>
 
-                    </ul>
+                        <p className='p-2'>CDO -- Address wrong -- Address given by cx -- as seen in recent address -- cx had not previously received order at that address  -- more than 500 m & also more than 4 km -- Not Serviceable -- ask rider to deliver at cx correct address -- rider disagree with the extra payout -- order cancel by informing cx -- same share to rider</p>
+
+                    </div>
                     <br />
 
                     <h3 className="text-md font-semibold text-emerald-300 mb-4 mt-4">CDO : Address wrong // Other State or District </h3>
-                    <ul className="list-disc pl-5">
-                        <li className='p-2'>CDO -- Address wrong -- Address given by cx -- <strong className='text-green-400'>Not Serviceable
-                        </strong> -- other state/District -- order cancel by informing the cx -- same share to rider</li>
+                    <div className="list-disc pl-5">
+                        <p className='p-2'>CDO -- Address wrong -- Address given by cx -- <strong className='text-green-400'>Not Serviceable
+                        </strong> -- other state/District -- order cancel by informing the cx -- same share to rider</p>
 
-                        <li className='p-2'>CDO -- Address wrong -- Address given by cx -- <strong className='text-green-400'>Not Serviceable
-                        </strong> -- other state/District -- order cancel -- order cancel -- refund TAT sahre to cx -- same share to rider</li>
-
-                        <hr className=' mt-8 mb-8' />
-
-                        <li className='p-2'>CDO -- Address wrong -- Address given by cx -- <strong className='text-green-400'>Mention Distance here -- Not Serviceable
-                        </strong> -- ask rider to deliver at cx correct address -- rider agree with the extra payout -- ask to raise a ticket -- Call close
-                        </li>
-
-                        <li className='p-2'>CDO -- Address wrong -- Address given by cx -- <strong className='text-green-400'>Mention Distance here -- Not Serviceable
-                        </strong> -- ask rider to deliver at cx correct address -- rider disagree with the extra payout -- order cancel by informing cx -- same share to rider </li>
-
-                        <li className='p-2'>CDO -- Address wrong -- Address given by cx -- <strong className='text-green-400'>Mention Distance here -- Not Serviceable
-                        </strong> -- ask rider to deliver at cx correct address -- rider disagree with the extra payout -- order cancel -- refund TAT sahre to cx -- same share to rider</li>
+                        <p className='p-2'>CDO -- Address wrong -- Address given by cx -- <strong className='text-green-400'>Not Serviceable
+                        </strong> -- other state/District -- order cancel -- order cancel -- refund TAT sahre to cx -- same share to rider</p>
 
 
-                    </ul>
+                    </div>
                 </div>
 
                 <div className="p-3 bg-gray-700 rounded-md shadow">
@@ -86,14 +73,14 @@ const CDO = () => {
                         <li className='p-2'>CDO -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- Error in OS in call recording -- ask DP to follow automation in CNR if cx wants to cancel else deliver the order</li>
                     </ul>
 
-                     <hr className='mb-5 mt-5' />
+                    <hr className='mb-5 mt-5' />
                     <div className="text-sm border border-green-400 mb-4 mt-2 p-2">
-              
-                    <p className="text-sm mb-2 mt-4 p-2">CDO -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- No proper call recording available -- <u>ETA+15 mins not crossed</u> -- 2 CNR attempt available ✅-- inform rider order got cancelled automatically & requested to wait till ETA + 15min.</p>
 
-                    <p className="text-sm mb-2 mt-4 p-2">CDO -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- No proper call recording available -- ETA+15 mins crossed -- 2 CNR attempt available ✅-- ask rider to wait for few moment order got cancelled automatically.</p>
+                        <p className="text-sm mb-2 mt-4 p-2">CDO -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- No proper call recording available -- <u>ETA+15 mins not crossed</u> -- 2 CNR attempt available ✅-- inform rider order got cancelled automatically & requested to wait till ETA + 15min.</p>
 
-                    <p className="text-sm mb-2 mt-4 p-2">CDO -- called cx -- <strong className='text-red-400'>cx not responsive <u>// Voicemail or Switched off</u> </strong> -- No proper call recording available -- ETA+15 mins crossed -- 2 CNR attempt available ✅-- order canceled by me -- same info shared to DP</p>
+                        <p className="text-sm mb-2 mt-4 p-2">CDO -- called cx -- <strong className='text-red-400'>cx not responsive</strong> -- No proper call recording available -- ETA+15 mins crossed -- 2 CNR attempt available ✅-- ask rider to wait for few moment order got cancelled automatically.</p>
+
+                        <p className="text-sm mb-2 mt-4 p-2">CDO -- called cx -- <strong className='text-red-400'>cx not responsive <u>// Voicemail or Switched off</u> </strong> -- No proper call recording available -- ETA+15 mins crossed -- 2 CNR attempt available ✅-- order canceled by me -- same info shared to DP</p>
 
                     </div>
                 </div>
@@ -104,6 +91,7 @@ const CDO = () => {
 };
 
 export default CDO;
+
 
 
 
